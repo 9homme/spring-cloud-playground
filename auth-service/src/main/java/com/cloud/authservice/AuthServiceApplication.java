0b257b795f65962d40e5c6c207c99987f5ace782
Saravuth Ruangsutham <saravuth.r@kbtg.tech>
2018-08-30 10:48:31 +0700

    Add method to logout and revoke token

--- auth-service/src/main/java/com/cloud/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/cloud/authservice/AuthServiceApplication.java
@@ -3,8 +3,12 @@ package com.cloud.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @SpringBootApplication
 @EnableAuthorizationServer
@@ -14,5 +18,19 @@ public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+	    return new InMemoryTokenStore();
+	}
+
+	@Bean
+	public DefaultTokenServices tokenServices() {
+	    final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+	    defaultTokenServices.setAccessTokenValiditySeconds(-1);
+
+	    defaultTokenServices.setTokenStore(tokenStore());
+	    return defaultTokenServices;
 	}
 }
--- auth-service/src/main/java/com/cloud/authservice/UserControler.java
+++ auth-service/src/main/java/com/cloud/authservice/UserControler.java
@@ -2,15 +2,34 @@ package com.cloud.authservice;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserControler {
 
+	@Autowired
+    private DefaultTokenServices tokenServices;
+
+    @Autowired
+    private TokenStore tokenStore;
+    	
 	@RequestMapping("/user")
     public Principal user(Principal user) {
 		System.out.println("user ==================>"+user.getName());
         return user;
+    }
+	
+	@RequestMapping("/user/logout")
+    public void revokeToken() {
+        final OAuth2Authentication auth = (OAuth2Authentication) SecurityContextHolder
+                .getContext().getAuthentication();
+        final String token = tokenStore.getAccessToken(auth).getValue();
+        tokenServices.revokeToken(token);
     }
 }
