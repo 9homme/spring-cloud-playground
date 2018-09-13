a41434b8ee4e94d508e2ed1801cb280f1586ee08
Saravuth Ruangsutham <saravuth.r@kbtg.tech>
2018-09-13 10:16:09 +0700

    Add hystrix command support for fallback method

--- product-service/src/main/java/com/cloud/productservice/ProductController.java
+++ product-service/src/main/java/com/cloud/productservice/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import lombok.Data;
 
 @RestController
@@ -17,11 +19,18 @@ public class ProductController {
 	private OAuth2RestTemplate  restTemplate;
 
     @GetMapping("/search")
+    @HystrixCommand(fallbackMethod = "getFallbackProductsByTypeAndName")
     public Product getProductsByTypeAndName(@RequestParam(value = "sku") final String sku) {
         String url = "http://PRICING-SERVICE/price?sku=" + sku;
         return restTemplate.getForObject(url, Product.class);
     }
-
+    
+    public Product getFallbackProductsByTypeAndName(final String sku) {
+        Product fallback = new Product();
+        fallback.setSku(sku);
+        fallback.setPrice("999.99");
+        return fallback;
+    }
 }
 
 @Data
